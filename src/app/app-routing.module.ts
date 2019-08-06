16839d2a0dbc37a294de7fa3f3722ff5e1925897
mnallinarayanan <rsekartest@gmail.com>
2019-08-06 19:31:40 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { MainComponent } from './layout/main/main.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [{
+  path:'',
+  component:MainComponent,
+  children:[
+    {path:'',component:HomeComponent,pathMatch:'full'}
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BootstrapModule } from './bootstrap/bootstrap.module';
+import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MainComponent } from './layout/main/main.component';
+import { LayoutModule } from './layout/layout.module';
+import { HomeModule } from './home/home.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BootstrapModule,
+    MaterialModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
